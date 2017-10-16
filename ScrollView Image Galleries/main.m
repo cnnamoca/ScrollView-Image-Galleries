1c2f2c86baf2628b0e34daa2eccfc953d1ed20cb
Carlo <carlo.namoca@gmail.com>
2017-10-16 13:36:51 -0700

    Initial Commit

---
+++ ScrollView Image Galleries/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ScrollView Image Galleries
+//
+//  Created by Carlo Namoca on 2017-10-16.
+//  Copyright © 2017 Carlo Namoca. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
